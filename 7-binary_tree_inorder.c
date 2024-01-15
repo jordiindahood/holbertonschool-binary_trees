39db3c06dd62f6ffc5cd74829ca21e6cd0fb2947
jordiindahood <hazem.ouni123@gmail.com>
2024-01-15 16:18:56 +0100

    script 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,7 +5,7 @@
  *@func: void (*)(int)
  * Return: void
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
