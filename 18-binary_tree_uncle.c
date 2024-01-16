8af5682433da3a56d5c34344410fc5f280f075b7
jordiindahood <hazem.ouni123@gmail.com>
2024-01-16 15:15:23 +0100

    script 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - find the uncle of the node
+ *@node: const binary_tree_t *
+ * Return: binary_tree_t *
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
+/**
+ * binary_tree_sibling - find the sibling of the node
+ *@node: const binary_tree_t *
+ * Return: binary_tree_t *
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->left || !node->parent->right)
+		return (0);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
