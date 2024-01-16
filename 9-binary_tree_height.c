96e6e915b421d3f17ef7b3d2f90a66c8c1d5c2f2
jordiindahood <hazem.ouni123@gmail.com>
2024-01-16 12:42:02 +0100

    script 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - get height
+ *@tree: const binary_tree_t *
+ * Return: void
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right = 0, left = 0;
+
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+	if (right < left)
+		return (left);
+	else
+		return (right);
+	return (0);
+}
